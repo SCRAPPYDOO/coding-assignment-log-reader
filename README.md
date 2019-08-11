41000af24598a7e8fba1c97335005b6a09863f85
Marcin Makowski <marcin.scrappy.makowski@gmail.com>
2019-08-11 23:53:28 +0100

    updated readme  and  resource loading from path

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # coding-assignment-log-reader
+
+### Path Param to Log File ###
+I know that this should be in arg but i decided to put it in application properties to faster use
+
+path to log file
+will use default data set if not set
+
+Windows example:
+
+log:
+  data:
+    path: "E:/sample-data"
+
+### Path Param to Log File ###
+
+time difference between start and finish timestamp for events to mark as alert
+
+log:
+  alert:
+    difference_in_ms: 4
--- log-reader/src/main/java/com/log/batch/configuration/BatchConfiguration.java
+++ log-reader/src/main/java/com/log/batch/configuration/BatchConfiguration.java
@@ -5,6 +5,8 @@ import com.log.batch.dto.LogDto;
 import com.log.batch.processor.LogProcessor;
 import com.log.batch.mapper.LogMapper;
 import com.log.batch.writer.LogWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -17,32 +19,45 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.util.Objects;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(BatchConfiguration.class);
     private static final String LOG_READER_NAME = "logReader";
+    private static final String DEFAULT_LOG_DATA = "sample-data";
 
     private final JobBuilderFactory jobBuilderFactory;
 
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final String logDataPath;
+    private final Resource resource;
 
     public BatchConfiguration(final JobBuilderFactory jobBuilderFactory,
                               final StepBuilderFactory stepBuilderFactory,
-                              @Value("${log.data.path}") final String logDataPath) {
+                              @Value("${log.data.path}") final String logDataPath,
+                              final ResourceLoader resourceLoader) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
-        this.logDataPath = logDataPath;
+
+        if(Objects.isNull(logDataPath) || logDataPath.isEmpty()) {
+            logger.warn("property log.data.path is not set or its empty, loading default data set");
+            this.resource = new ClassPathResource(DEFAULT_LOG_DATA);
+        } else {
+            this.resource = resourceLoader.getResource(String.format("file:%s", logDataPath));
+        }
     }
 
     @Bean
     public FlatFileItemReader<LogDto> reader(final LogMapper logLineMapper) {
         return new FlatFileItemReaderBuilder<LogDto>()
                 .name(LOG_READER_NAME)
-                .resource(new ClassPathResource(logDataPath))
+                .resource(resource)
                 .lineMapper(logLineMapper)
                 .build();
     }
